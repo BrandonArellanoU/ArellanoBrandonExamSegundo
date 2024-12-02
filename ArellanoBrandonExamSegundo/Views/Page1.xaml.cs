51f19ce2abaf79f2590fe10bf519df02b68fd54d
Brandon Arellano <arebrandon12@gmail.com>
2024-12-02 08:58:22 -0500

    Funcionamiento del boton de guardar en la page1

--- ArellanoBrandonExamSegundo/Views/Page1.xaml.cs
+++ ArellanoBrandonExamSegundo/Views/Page1.xaml.cs
@@ -5,34 +5,13 @@ public partial class Page1 : ContentPage
 	public Page1()
 	{
 		InitializeComponent();
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
-
     }
 
     private async void Guardar_Clicked(object sender, EventArgs e)
     {
-        if (BindingContext is Models.Page1 numero)
-            File.WriteAllText(TexNombre.file, TextEditorNumero.Text);
-            File.WriteAllText(nombre.file, TextEditorNombre.Text);
-
+        if (BindingContext is Models.Page1 Page1)
+            File.WriteAllText(TextEditor.Text, TextEditorNombre.Text);
+          
         await Shell.Current.GoToAsync("..");
-    }
-
-
-    private void LoadNote(string fileName)
-    {
-        Models.Page1 text = new Models.Page1;
-        text.file = fileName;
-
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
     }
 }
