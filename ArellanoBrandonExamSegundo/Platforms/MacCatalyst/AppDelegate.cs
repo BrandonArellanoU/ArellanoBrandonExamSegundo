079d3a39fac5e59a7b714278789d5d517582f097
Brandon Arellano <arebrandon12@gmail.com>
2024-12-02 07:17:48 -0500

    Agregar archivos de proyecto.

---
+++ ArellanoBrandonExamSegundo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ArellanoBrandonExamSegundo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
